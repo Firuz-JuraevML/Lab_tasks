81b859ed8da3eab78f577edeefbcb9b16f5de9ec
Firuz Juraev <62535926+Firuz-JuraevML@users.noreply.github.com>
2020-07-21 21:35:05 +0500

    Create joining_csv_files.py
    Joining many csv files in second

---
+++ joining_csv_files.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import glob
+from os import listdir
+
+
+def list_files(path):
+    files = []
+    counter = 0
+    for f in listdir("./" + path + "/"):
+        if f.endswith('.' + "csv"):
+            files.append(f)
+            counter = counter + 1
+    print ("CSV Files: " + str(counter))
+    return files
+
+
+if __name__ == '__main__':
+    paths = ['1', '2']
+    for path in paths:
+        print ("Folder: " + path)
+        li = []
+
+        all_files = list_files(path)
+
+        for filename in all_files:
+            filepath = "./" + path + "/" + filename
+            df = pd.read_csv( filepath, index_col=None, header=0)
+            li.append(df)
+
+        frame = pd.concat(li, axis=0, ignore_index=True)
+
+        frame.to_csv("coronovirus.csv")
